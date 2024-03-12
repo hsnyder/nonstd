0efec8e5e8aaa14f40bd4adb573a375b5cb29d26
Harris M Snyder <harris.snyder@gmail.com>
2024-03-12 11:34:24 -0400

    cleanup

--- nonstd.h
+++ nonstd.h
@@ -3,45 +3,10 @@
 	This is free and unencumbered software released into the public domain.
 
 	The C standard library isn't great. Nonstd is my attempt to fill in some
-	of the gaps and increase the convenience of programming in C. It consists
-	of three distinct parts:
+	of the gaps and increase the convenience of programming in C.
 
-	- nonstd_arch.h, which contains features that have OS-specific and 
-	  architecture-specific code which may not be implemented on all archs.
-	- nonstd_base.h, which contains most of the library. It contains some
-	  OS-specific code, but nothing that requries a specific compiler or arch.
-	- nonstd_str.h, which contains a number of tools to work with strings,
-	  and nothing OS or architecture specific.
-
-	Those three parts can be used independently of each other, for example if
-	some of the features aren't implemented on your architecture, you can omit
-	nonstd_arch. However, nonstd_base has some features that are disabled if
-	nonstd_arch isn't being used, and likewise nonstd_str has some disabled
-	features if nonstd_base isn't being used. Unless you have reason to do 
-	otherwise, just include "nonstd.h", which includes all three.
-
-	If you need to manually include a subset of these, make sure you 
-	include them in order: nonstd_arch before nonstd_base before nonstd_str.
-	This will make sure as many optional features as possible are enabled.
-
-	These files are single-header libraries (credit to Sean Barrett for the
-	idea). They include both the header and the actual definitions in
-	a single file. To use them, copy the .h files into your project and 
-	define NONSTD_IMPLEMENTATION in exactly one .c file, immediately before 
-	you include nonstd.h.
-
-	You can find the documentation for each sub-library in the header portion
-	of each file. 
 */
 
-
-/*
-	Harris M. Snyder, 2023
-	This is free and unencumbered software released into the public domain.
-
-	nonstd_base.h is part of 'nonstd': an attempt to supplement the C 
-	standard library. See the comments in `nonstd.h` for an overview.
-*/
 #ifndef NONSTD_H
 #define NONSTD_H
 
@@ -296,18 +261,12 @@ NONSTD_API  void warning_message (char * str);
 NONSTD_API  void info_message    (char * str);
 
 
+/* 
+   ============================================================================
+		ARENA MEMORY MANAGEMENT
+   ============================================================================
 
-
-/*
-	Harris M. Snyder, 2023
-	This is free and unencumbered software released into the public domain.
-
-	nonstd_arena.h is part of 'nonstd': an attempt to supplement the C 
-	standard library. See the comments in `nonstd.h` for an overview.
-
-	nonstd_arena.h provides a simple arena allocator and a hash map to go with it.
-	Credit to Chris Wellons (https://nullprogram.com/) and NRK (https://nrk.neocities.org/) 
-	for the hash map design.
+   Some ideas drawn from Chris Wellons's excellent blog (https://nullprogram.com/) 
 */
 
 #include <stddef.h>
@@ -366,6 +325,17 @@ NONSTD_API char *arena_strdup(Arena *a, char *s);
 // null-terminated.
 
 
+/* 
+   ============================================================================
+		HASH MAPS
+   ============================================================================
+
+   Credit to Chris Wellons (https://nullprogram.com/) and NRK (https://nrk.neocities.org/) 
+   for the hash map design.
+
+*/
+
+#include <stddef.h>
 
 ///////////   HASH MAPS  ////////////
 // 
@@ -500,6 +470,11 @@ NONSTD_API  OrderedHashMap *ordered_hash_map_list_reverse(OrderedHashMap *list);
 
 
 
+/* 
+   ============================================================================
+		STRING TOOLS
+   ============================================================================
+*/
 
 
 ///////////   GENERAL ASCII TOOLS
