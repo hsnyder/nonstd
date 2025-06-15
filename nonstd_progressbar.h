7f5d6f9748a5ed0f865d2151e0a780eb4d982dc1
Harris M. Snyder <harris.snyder@gmail.com>
2025-06-15 00:09:34 -0400

    progress bar improvements

--- nonstd_progressbar.h
+++ nonstd_progressbar.h
@@ -4,16 +4,19 @@
 #include <stdio.h>
 #include <stdint.h>
 typedef struct {
-        // you can update these at any time
-        int current, maximum;  // required for the bar to work 
-			       // if maximum is 0, only rate info printed
-        char prefix_label[32]; // optional, safe to leave zero
-        char suffix_label[64]; // optional, safe to leave zero
-        FILE *output_stream;   // optional, defaults to stderr
+    // you can update these at any time
+    int current, maximum;  // required for the bar to work
+		       // if maximum is 0, only rate info printed
+    char prefix_label[32]; // optional, safe to leave zero
+    char suffix_label[64]; // optional, safe to leave zero
+    FILE *output_stream;   // optional, defaults to stderr
+	int always_show_absolute_progress;
 
-        // don't manually edit these
-        int last_current, last_output_len;
-        uint64_t last_update_time;
+    // don't manually edit these
+    int last_current, last_output_len;
+	int first_update_progress;
+    uint64_t first_update_time;
+	uint64_t last_update_time;
 } ProgressBar;
 
 int progress_bar_ratelimit(ProgressBar *b);
@@ -75,23 +78,23 @@ int main(void)
 int
 progress_bar_ratelimit(ProgressBar *b)
 {
-        uint64_t delta_ticks = read_cpu_timer() - b->last_update_time;
-        float delta_t = cpu_time_to_sec(delta_ticks);
-        return delta_t*1000.0f >= NONSTD_PROGRESSBAR_RATELIMIT_TIME_MS;
+    uint64_t delta_ticks = read_cpu_timer() - b->last_update_time;
+    float delta_t = cpu_time_to_sec(delta_ticks);
+    return delta_t*1000.0f >= NONSTD_PROGRESSBAR_RATELIMIT_TIME_MS;
 }
 
 void 
 progress_bar_print(ProgressBar *b)
 {
-        uint64_t now = read_cpu_timer();
+    uint64_t now = read_cpu_timer();
 
-        FILE * f = b->output_stream ? b->output_stream : stderr;
-        for (int i = 0; i < b->last_output_len; i++)
-                fputc('\b',f);
+    FILE * f = b->output_stream ? b->output_stream : stderr;
+    for (int i = 0; i < b->last_output_len; i++)
+            fputc('\b',f);
 
-	uint64_t delta_ticks = now - b->last_update_time;
+	uint64_t delta_ticks = now - b->first_update_time;
 	float delta_seconds = cpu_time_to_sec(delta_ticks);
-	int delta_iters = b->current - b->last_current;
+	int delta_iters = b->current - b->first_update_progress;
 	float it_per_s = delta_iters / delta_seconds;
 
 	int n = 0;
@@ -101,14 +104,37 @@ progress_bar_print(ProgressBar *b)
 		char bar[61] = {0};
 		for (int i = 0; i < 60; i++)
 			bar[i] = i <= cutoff ? NONSTD_PROGRESSBAR_FILL_CHARACTER : ' ';
-		n = fprintf(f, "%s[%s] (%.2f it/s) %s", b->prefix_label, bar, it_per_s, b->suffix_label);
+		if (it_per_s > 1.0f) {
+			if (b->always_show_absolute_progress) {
+				n = fprintf(f, "%s[%s] %i/%i (%.2f it/s) %s", b->prefix_label, bar, b->current, b->maximum, it_per_s, b->suffix_label);
+			} else {
+				n = fprintf(f, "%s[%s] (%.2f it/s) %s", b->prefix_label, bar, it_per_s, b->suffix_label);
+			}
+		} else {
+			float s_per_it = 1.0f / it_per_s;
+			if (b->always_show_absolute_progress) {
+				n = fprintf(f, "%s[%s] %i/%i (%.2f s/it) %s", b->prefix_label, bar, b->current, b->maximum, s_per_it, b->suffix_label);
+			} else {
+				n = fprintf(f, "%s[%s] (%.2f s/it) %s", b->prefix_label, bar, s_per_it, b->suffix_label);
+			}
+		}
 	} else {
-		n = fprintf(f, "%s %iit (%.2f it/s) %s", b->prefix_label, b->current, it_per_s, b->suffix_label);
+		if (it_per_s > 1.0f) {
+			n = fprintf(f, "%s %iit (%.2f it/s) %s", b->prefix_label, b->current, it_per_s, b->suffix_label);
+		} else {
+			float s_per_it = 1.0f / it_per_s;
+			n = fprintf(f, "%s %iit (%.2f s/it) %s", b->prefix_label, b->current, s_per_it, b->suffix_label);
+		}
 	}
 
-        b->last_current = b->current;
-        b->last_output_len = (n > 0) ? n : 0;
-        b->last_update_time = now;
+    b->last_current = b->current;
+    b->last_output_len = (n > 0) ? n : 0;
+	b->last_update_time = now;
+	if(b->first_update_time == 0) {
+		b->first_update_time = now;
+		b->first_update_progress = b->current;
+	}
+
 }
 #endif
 
